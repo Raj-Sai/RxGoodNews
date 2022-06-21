ce5f83d244d5cff75c272601b43625168f8558f7
AmsarajMariyappan <91876932+AmsarajMariyappan@users.noreply.github.com>
2022-06-21 19:51:27 +0700

    Initial Commit

---
+++ RxGoodNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxGoodNews
+//
+//  Created by Amsaraj M on 21/6/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
